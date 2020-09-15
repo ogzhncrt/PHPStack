2afbbc852d7b1a319e9ad961ccb81b39474df9aa
Oğuzhan Cerit <ogzhncrt@gmail.com>
2020-09-15 13:55:20 +0300

    Update PHPStack.php

--- PHPStack.php
+++ PHPStack.php
@@ -2,7 +2,8 @@
 
 class PHPStack {
 
-	private $stack = array();
+	private $stack    = array();
+	private $reversed = 0;
 	
 	public function push($item) {
 		array_push($this->stack, $item);
@@ -12,8 +13,25 @@ class PHPStack {
 		return array_pop($this->stack);
 	}
 
+	public function top() {
+		return end($this->stack);
+	}
+
+	public function size() {
+		return count($this->stack);
+	}
+
+	public function reverse(){
+		$this->stack    = array_reverse($this->stack,FALSE);
+		$this->reversed = !$this->reversed;
+	}
+
 	public function isEmpty() {
 		return empty($this->stack);
+	}
+
+	public function isReversed() {
+		return $this->reversed;
 	}
 }
 
