743ede72b515d42531be612656daac4e3c6e457f
Oğuzhan Cerit <ogzhncrt@gmail.com>
2020-09-15 13:47:50 +0300

    Update PHPStack.php

--- PHPStack.php
+++ PHPStack.php
@@ -4,6 +4,17 @@ class PHPStack {
 
 	private $stack = array();
 	
+	public function push($item) {
+		array_push($this->stack, $item);
+	}
+
+	public function pop($item) {
+		return array_pop($this->stack);
+	}
+
+	public function isEmpty() {
+		return empty($this->stack);
+	}
 }
 
 ?>
