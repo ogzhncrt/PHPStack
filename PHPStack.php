e466c139485c3dd43445675527a5f33b422d8267
Oğuzhan Cerit <ogzhncrt@gmail.com>
2020-09-16 16:39:24 +0300

    1st Step to keep history

--- PHPStack.php
+++ PHPStack.php
@@ -7,19 +7,23 @@ class PHPStack {
 
 	private $stack    = array();
 	private $reversed = 0;
+	private $history  = array();
 	
 	public function push($item) {
 		/*
 			Inserts a new element at the top of the current stack.
 		*/
 		array_push($this->stack, $item);
+		$this->addHistory(__FUNCTION__,$item);
 	}
 
 	public function pop() {
 		/*
 			Returns top element of the stack and remove it from the current stack.
 		*/
-		return array_pop($this->stack);
+		$item = array_pop($this->stack);
+		$this->addHistory(__FUNCTION__,$item);
+		return $item;
 	}
 
 	public function top() {
@@ -40,20 +44,24 @@ class PHPStack {
 		/* 
 			This function reverses the current stack. 
 		*/
+		$this->addHistory(__FUNCTION__);
 		$this->stack    = array_reverse($this->stack,FALSE);
 		$this->reversed = !$this->reversed;
 	}
 
+	
 	public function returnToOriginal(){
 		/*
 			This function reverses the current stack if it is already reversed. 
 			If the current stack is not reversed, will do nothing.
 		*/
 		if($this->reversed){
+			$this->addHistory("reverse");
 			$this->stack    = array_reverse($this->stack,FALSE);
 			$this->reversed = 0;
 		}
 	}
+
 
 	public function isEmpty() {
 		/*
@@ -67,6 +75,10 @@ class PHPStack {
 			This function returns the current stack was previously reversed or not. 
 		*/
 		return $this->reversed;
+	}
+
+	private function addHistory($mode,$item = 0){
+		array_push($this->history, array($mode,$item));
 	}
 }
 
